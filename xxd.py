9c2e423f943a70c5d03573a3070dfa96c1aa202d
DCKahagalla <30688349+DCKahagalla@users.noreply.github.com>
2018-07-30 16:06:34 +0530

    Add files via upload

---
+++ xxd.py
@@ -0,0 +1,24 @@
+f=open('xxd.py','r')
+s=0	
+while True:
+	r=f.read(16)
+	d=r
+	if not r:
+		break
+	he=[]
+	for i in r:
+		he.append('%02x'%ord(i))
+	he2=[]
+	for z in range(0,len(he),2):
+		he2.append(''.join((he[z:z+2])))
+	ch=[]
+	for c in d:
+		cc=ord(c)
+		if cc<32:
+			ch.append('.')
+		else:
+			ch.append(c)
+	step=('%07x'%(s*16))
+	print('{0}: {1:<39} {2}'.format(step,''.join(he2),''.join(ch)))
+	s=s+1
+		
