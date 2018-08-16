6ea0fa738be86473a2df3d37c2028caad521a836
DCKahagalla <30688349+DCKahagalla@users.noreply.github.com>
2018-08-15 23:42:29 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,12 @@
-import socket
+import socket ,sys ,os
+host = "192.168.150.128"
+port = 5555
 
-s = socket.socket()
-host = socket.gethostname()
-port = input("enter Port: ")
-
-s.connect((host, port))
-print (s.recv(1024))
-s.close
+c= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+c.connect((host, port))
+print 'Connected'
+while True:
+	data= c.recv(1024)
+	print data
+	send_data=raw_input('\nclient : ')
+	c.send(send_data)
